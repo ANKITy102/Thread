b39905a9587bf94f935bb8d2ebb3a371b7884975
Ankit Yadav <mahesh9426346889@gmail.com>
2024-01-31 23:30:04 +0530

    initial commit

---
+++ app/(root)/activity/loading.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <>
+    <h1 className='head-text'>Activity</h1>
+
+      <section className='mt-10 flex flex-col gap-5'>
+          <p className='!text-base-regular text-light-3'>No activity yet.. Please Wait.</p>
+      </section>
+          </>
+  )
+}
+
+export default loading
